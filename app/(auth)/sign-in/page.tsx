b4d5dd2a4e24db317c7b47d2338e110e2c8c7aa4
Aldi <aldiirawan748@gmail.com>
2025-06-11 23:26:38 +0700

    finalll

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,10 @@
+import { Suspense } from "react";
 import SigninPage from "./_components/SigninPage";
 
 export default function LoginPage() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <SigninPage />
-    </>
+    </Suspense>
   );
 }
